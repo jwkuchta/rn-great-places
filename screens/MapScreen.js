0363c7b3f991ae1395d39c9b76f3c1a62790c778
Justina Kuchta <justyna.kuchta@gmail.com>
2020-09-13 15:01:03 -0400

    marker is places on the map on click/tap

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 
 const MapScreen = props => {
+
+    const [ selectedLocation, setSelectedLocation ] = useState()
 
     // this region object requires the following args and they have to be names as seen below
     const mapRegion = {
@@ -12,11 +14,31 @@ const MapScreen = props => {
         longitudeDelta: 0.0421
     }
 
+    const selectLocationHandler = (event) => {
+        setSelectedLocation({
+            lat: event.nativeEvent.coordinate.latitude,
+            long: event.nativeEvent.coordinate.longitude
+        })
+    }
+
+    let markerCoordinates
+
+    if (selectedLocation) {
+        console.log(selectedLocation)
+        markerCoordinates = {
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.long
+        }
+    }
+
     return (
         <MapView
         style={styles.map}
         region={mapRegion} 
-        />
+        onPress={selectLocationHandler}
+        >
+            {markerCoordinates && <Marker title="picked location" coordinate={markerCoordinates}></Marker>}
+        </MapView>
     )
 }
 
